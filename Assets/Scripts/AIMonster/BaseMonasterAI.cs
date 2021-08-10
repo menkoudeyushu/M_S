d9cff80d156b2017f1bae8540b8723ba6d645094
CMQ <arsnowyizumi@126.com>
2021-08-10 23:17:27 +0800

    删除多余的包

--- Assets/Scripts/AIMonster/BaseMonasterAI.cs
+++ Assets/Scripts/AIMonster/BaseMonasterAI.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+[SerializeField]
 public  class BaseMonasterAI:MonoBehaviour
 {
-    private Transform monaster_transform;
-    public Transform MonasterTransform { get; }
+    private GameObject _game_object;
+    public GameObject  Game_object{ get; }
+
+    //每一个的monster AI 都移有一个Animator
+    private Animator[] Npc_animator;
+
+    private void Awake()
+    {
+        Npc_animator = GetComponentsInChildren<Animator>();
+        if (Npc_animator == null)
+            Debug.LogError("npc ai do not find animator");
+    }
     
 
-    //functions
-    
 }
